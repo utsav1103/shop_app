8394b1ef2d43e5f5f8f420df58bdc523cd13c447
utsav-baghela <utsavu858@gmail.com>
2023-08-28 17:03:50 +0530

    using Dropdown Button

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:shop_app/data/categories.dart';
+import 'package:shop_app/models/category.dart';
 
 class NewItem extends StatefulWidget {
   const NewItem({super.key});
@@ -26,9 +28,45 @@ class _NewItemState extends State<NewItem> {
                 decoration: InputDecoration(
                   label: Text('Name'),
                 ),
-                validator: (value){
+                validator: (value) {
                   return 'Demo....';
                 },
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                        label: Text('Quantity'),
+                      ),
+                      initialValue: '1',
+                    ),
+                  ),
+                  const SizedBox(width: 8),
+                  Expanded(
+                    child: DropdownButtonFormField(
+                      items: [
+                        for (final Category in categories.entries)
+                          DropdownMenuItem(
+                            value: Category.value,
+                            child: Row(
+                              children: [
+                                Container(
+                                  width: 16,
+                                  height: 16,
+                                  color: Category.value.color,
+                                ),
+                                const SizedBox(width: 6),
+                                Text(Category.value.title),
+                              ],
+                            ),
+                          ),
+                      ],
+                      onChanged: (value) {},
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
